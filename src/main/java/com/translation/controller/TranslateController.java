cd3fee90d54e037924fcaf6d7780b7b902c27b96
m-abdullah028 <abdullah.official028@gmail.com>
2025-03-27 18:25:53 +0100

    created service and controller classes

---
+++ src/main/java/com/translation/controller/TranslateController.java
@@ -0,0 +1,5 @@
+package com.translation.controller;
+
+public class TranslateController {
+
+}
---
+++ src/main/java/com/translation/service/TranslateService.java
@@ -0,0 +1,15 @@
+package com.translation.service;
+
+import com.google.gson.JsonObject;
+
+public class TranslateService {
+	public JsonObject translateString(JsonObject requestJson) {
+		if (requestJson.has("query") && !requestJson.get("query").getAsString().isEmpty() && requestJson.has("source")
+				&& !requestJson.get("source").getAsString().isEmpty() && requestJson.has("target")
+				&& !requestJson.get("target").getAsString().isEmpty()) {
+			
+
+		}
+
+	}
+}
