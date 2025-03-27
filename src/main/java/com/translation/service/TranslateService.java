22153d25f506a3d136bd854374d1fea6e36bf3b6
m-abdullah028 <abdullah.official028@gmail.com>
2025-03-27 18:30:39 +0100

    data fetched from request

--- src/main/java/com/translation/service/TranslateService.java
+++ src/main/java/com/translation/service/TranslateService.java
@@ -7,8 +7,9 @@ public class TranslateService {
 		if (requestJson.has("query") && !requestJson.get("query").getAsString().isEmpty() && requestJson.has("source")
 				&& !requestJson.get("source").getAsString().isEmpty() && requestJson.has("target")
 				&& !requestJson.get("target").getAsString().isEmpty()) {
-			
-
+			String query = requestJson.get("query").getAsString();
+			String source = requestJson.get("source").getAsString();
+			String target = requestJson.get("target").getAsString();
 		}
 
 	}
